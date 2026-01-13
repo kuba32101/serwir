166789af3df81020adb98bff997828e5939baec2
kuba32101 <kubayt059@gmail.com>
2026-01-13 15:46:57 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -12,22 +12,20 @@ shared_text = ""
 @app.route('/')
 def gra_mafijna():
     return render_template('index.html')
-
+    
 @socketio.on('connect')
 def handle_connect():
     print('Nowe połączenie')
-    emit('update_text', shared_text)
 
-@socketio.on('text_changed')
-def handle_text_changed(data):
-    global shared_text
-    shared_text = data.get('text', '')
-    if shared_text == "":
-        return 
-    else:
-        emit('update_text', shared_text, broadcast=True)
+@socketio.on('update_text')
+def update_history(data):
+    inp = data.get('inp')
+    text = data.get('text')
+    if not inp or not text:
+        return
+    
+    emit('update_history', {'inp': inp, 'text': text}, broadcast=True)
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
-
     socketio.run(app, host='0.0.0.0', port=port)
